fa2a0523f4df26ede3b12bee3fb9fb7ca09bfc52
NY <785539058@qq.com>
2025-08-25 11:47:32 +0800

    fix: correct dataset filename path in benchmark script

--- benchmark/fashion-mnist-784-euclidean/benchmark_fashion_mnist.py
+++ benchmark/fashion-mnist-784-euclidean/benchmark_fashion_mnist.py
@@ -30,7 +30,7 @@ from client import OasisDBClient, OasisDBError
 # --- 1. 配置信息 ---
 # 数据集配置
 DATASET_URL = "http://ann-benchmarks.com/fashion-mnist-784-euclidean.hdf5"
-DATASET_FILENAME = "benchmark/fashion-mnist-784-euclidean/data/fashion-mnist-784-euclidean.hdf5"
+DATASET_FILENAME = "benchmark/fashion-mnist-784-euclidean/dataset/fashion-mnist-784-euclidean.hdf5"
 # OasisDB 配置
 COLLECTION_NAME = "fashion_mnist"
 VECTOR_DIMENSION = 784  # Fashion-MNIST 向量是 784 维
